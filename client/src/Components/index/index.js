10030e32c280acfcb65df2c4b65022f68c9e81fa
eshan1799 <eshan1799@gmail.com>
2020-10-21 16:12:06 +0100

    add export index for all components

---
+++ client/src/Components/index/index.js
@@ -0,0 +1,8 @@
+export { default as LandingPage } from '../LandingPage';
+export { default as Login } from '../Login';
+export { default as Register } from '../Register';
+
+export { default as Error404 } from '../Error404';
+
+export { default as PrivateRoute } from '../PrivateRoute';
+export { default as UnauthRoute } from '../UnauthRoute';
