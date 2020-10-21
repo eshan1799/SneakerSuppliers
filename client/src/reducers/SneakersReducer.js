393562244043a487d73f4b75274b525a49ff0c4a
eshan1799 <eshan1799@gmail.com>
2020-10-21 18:20:00 +0100

    created ADD_POSTS action type

--- client/src/reducers/SneakersReducer.js
+++ client/src/reducers/SneakersReducer.js
@@ -1,34 +1,27 @@
 const initState = {
     username: '',
-    cash: 0,
-    equity: 0,
-    balance: 0,
-    portfolio: [],
-    history: [],
-    searchResult: {},
-    historicPrices: [],
-    news: [],
-    comparison: []
+    myPosts: [],
+    allPosts: []
 }
 
 const SneakersReducer = (state = initState, action) => {
     switch (action.type) {
-        case 'ADD_PORTFOLIO':
-            return { ...state, cash: action.payload.cash.toFixed(2), equity: action.payload.equity.toFixed(2), balance: (action.payload.cash + action.payload.equity).toFixed(2), portfolio: action.payload.portfolio, username: action.payload.username }
-        case 'ADD_HISTORY':
-            return { ...state, history: action.payload }
-        case 'ADD_SEARCH':
-            return { ...state, searchResult: action.payload }
-        case 'ADD_HISTORIC_PRICES':
-            return { ...state, historicPrices: action.payload }
-        case 'ADD_NEWS':
-            return { ...state, news: action.payload}
-        case 'ADD_COMPARISON':
-            return { ...state, comparison: action.payload }
-        case 'CLEAR_SEARCH':
-            return { ...state, searchResult: {}, historicPrices: [], news: []}
-        case 'LOG_OUT':
-            return state = initState
+        case 'ADD_POSTS':
+            return { ...state, allPosts: action.payload, username: action.payload.username }
+        // case 'ADD_HISTORY':
+        //     return { ...state, history: action.payload }
+        // case 'ADD_SEARCH':
+        //     return { ...state, searchResult: action.payload }
+        // case 'ADD_HISTORIC_PRICES':
+        //     return { ...state, historicPrices: action.payload }
+        // case 'ADD_NEWS':
+        //     return { ...state, news: action.payload}
+        // case 'ADD_COMPARISON':
+        //     return { ...state, comparison: action.payload }
+        // case 'CLEAR_SEARCH':
+        //     return { ...state, searchResult: {}, historicPrices: [], news: []}
+        // case 'LOG_OUT':
+        //     return state = initState
         default:
             return state;
     }
