dde115f6d12bd6b0a0fd1b5680dfd96a8d076ea9
eshan1799 <eshan1799@gmail.com>
2020-10-21 18:22:31 +0100

    connect Dashboard to the Redux store

--- client/src/Containers/Dashboard.js
+++ client/src/Containers/Dashboard.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 class Dashboard extends React.Component {
     render() {
         return (
             <>
+            <h1>Dashboard</h1>
             </>
         )
     }
 }
 
-export default Dashboard;
+const mSTP = state => ({
+    dashboard: state.portfolio,
+    equity: state.equity
+})
+
+export default connect(mSTP)(Dashboard)
