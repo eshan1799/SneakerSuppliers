c98c29ab5313ea135bdf7476f377aa21cc4deb18
eshan1799 <eshan1799@gmail.com>
2020-10-22 20:14:03 +0100

    display post data from the Redux store

--- client/src/Containers/Posts.js
+++ client/src/Containers/Posts.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { getPosts } from "../actions/Actions";
+import { Post } from "../Components/index/index";
 
 class Posts extends React.Component {
 
     componentDidMount() {
-        // this.props.getPosts();
+        this.props.getPosts();
+    }
+
+    renderPosts = posts => {
+        return posts.map((post, index) => {
+                return  <div key={ index }>
+                            <Post post={ post }/>
+                        </div>
+                }
+        )
     }
 
     render() {
-        this.props.getPosts();
+        // this.props.getPosts();
 
         return (
             <>
-            <h1>Posts</h1>
+            <h1>Sneaker Suppliers</h1>
+            <h2>Posts</h2>
+            { this.props.allPosts  ? this.renderPosts(this.props.allPosts) : 'There are no posts' }
             </>
         )
     }
@@ -23,4 +35,8 @@ const mDTP = dispatch => ({
     getPosts: () => dispatch(getPosts())
 })
 
-export default connect(null, mDTP)(Posts);
+const mSTP = state => ({
+    allPosts: state.allPosts
+})
+
+export default connect(mSTP, mDTP)(Posts);
