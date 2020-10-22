5ad7fd1b570e19d13435ae3a44c07f1d695dd913
eshan1799 <eshan1799@gmail.com>
2020-10-22 19:10:57 +0100

    dispatch getPosts() action on Posts page render

--- client/src/Containers/Posts.js
+++ client/src/Containers/Posts.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { getPosts } from "../actions/Actions";
 
 class Posts extends React.Component {
+
+    componentDidMount() {
+        // this.props.getPosts();
+    }
+
     render() {
+        this.props.getPosts();
+
         return (
             <>
             <h1>Posts</h1>
@@ -10,4 +19,8 @@ class Posts extends React.Component {
     }
 }
 
-export default Posts;
+const mDTP = dispatch => ({
+    getPosts: () => dispatch(getPosts())
+})
+
+export default connect(null, mDTP)(Posts);
